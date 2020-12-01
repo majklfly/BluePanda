243a59579a94a896d7ddc9e08119df5fda7f8052
majklfly <majklfly@gmail.com>
2020-12-01 20:43:11 +0000

    bluePanda 2.0 initial

---
+++ libs/php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+	$cd_user = "bluepanda"; // user name
+	$cd_password = "Q5HCXQdPuNbcdaV8"; // password
+	$cd_dbname = "companydirectory"; // database name
+
+?>
