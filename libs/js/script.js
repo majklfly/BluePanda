63a7fee9a61350b4e3869bacf97c4b61738d05cc
majklfly <majklfly@gmail.com>
2020-12-01 23:16:55 +0000

    added employees to the containers

--- libs/js/script.js
+++ libs/js/script.js
@@ -12,12 +12,46 @@ $("#checkboxContainer").on("click", function() {
 
 // ------- INITIAL LOAD -------
 
-// fetch all employees
+// fetch all departments
 $.ajax({
-    url: "libs/php/getAll.php",
+    url: "libs/php/getAllDepartments.php",
     type: "GET",
     dataType: "json",
     success: function(result) {
+        result.data.map((department, index) => {
+            console.log("department", department);
+            const card =
+                "<article class='card' style='max-width: 18rem;'><div class='card-header'>" +
+                department.name +
+                "</div><div class='card-content' id='department" +
+                department.name +
+                "'></div></article>";
+            $(card)
+                .appendTo("#cardsContainer")
+                .queue(function() {});
+        });
+        if ($(".card").length) {
+            console.log("updated");
+            $.ajax({
+                url: "libs/php/getAll.php",
+                type: "GET",
+                dataType: "json",
+                success: function(result) {
+                    console.log("employees", result);
+
+                    result.data.map((item) => {
+                        console.log($("#Sales").length);
+                        $("#department" + item.department).append(
+                            "<h3>" + item.firstName + " " + item.lastName + "</h3>"
+                        );
+                    });
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR);
+                },
+            });
+        }
+
         console.log(result);
     },
     error: function(jqXHR, textStatus, errorThrown) {
