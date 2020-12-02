41e65b4a76e3568002b52d9388caf7f56083b6b8
majklfly <majklfly@gmail.com>
2020-12-02 01:24:55 +0000

    connected database

--- libs/php/config.php
+++ libs/php/config.php
@@ -1,10 +1,10 @@
 <?php
 
-	$cd_host = "127.0.0.1";
+	$cd_host = "eu-cdbr-west-03.cleardb.net";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "bluepanda"; // user name
-	$cd_password = "Q5HCXQdPuNbcdaV8"; // password
-	$cd_dbname = "companydirectory"; // database name
+	$cd_user = "bbc6fb6419da99"; // user name
+	$cd_password = "320e91ab"; // password
+	$cd_dbname = "heroku_4b6a4ad885f7b56"; // database name
 
 ?>
